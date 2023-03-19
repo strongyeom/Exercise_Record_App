6df61f56425e83c9047afc63865a52a2ddd4cdf0
yeomsungpil <present7256@gmail.com>
2023-03-19 22:24:03 +0900

    feat: example text

--- ExerciseRecordApp/ExerciseRecordApp/ContentView.swift
+++ ExerciseRecordApp/ExerciseRecordApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("운동일지 앱 처음")
         }
         .padding()
     }
