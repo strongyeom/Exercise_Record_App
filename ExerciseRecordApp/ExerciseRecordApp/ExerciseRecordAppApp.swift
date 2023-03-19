26cc675f9de08ce99a4fd88d5da5322473272f85
yeomsungpil <present7256@gmail.com>
2023-03-19 22:21:27 +0900

    feat: first time App

---
+++ ExerciseRecordApp/ExerciseRecordApp/ExerciseRecordAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseRecordAppApp.swift
+//  ExerciseRecordApp
+//
+//  Created by 염성필 on 2023/03/19.
+//
+
+import SwiftUI
+
+@main
+struct ExerciseRecordAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
